60ac13e54b13135ac08ed6d67c2fd56f6202117a
M4dparzival <zachnathan12345@gmail.com>
2025-12-06 18:44:27 +0800

    Restore BST options: Find Height, Min, Max, traversals

--- algorithms/binarysearchtree.py
+++ algorithms/binarysearchtree.py
@@ -1,95 +1,141 @@
 class Node:
-    def __init__(self, value):
+    """A node in the Binary Search Tree."""
+
+    def __init__(self, value, left=None, right=None):
+        """Initialize a node with value and optional left/right children."""
         self.value = value
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
 
-class BinarySearchTree:
-    def __init__(self):
-        self.root = None
 
-    def insert(self, value):
-        if self.root is None:
-            self.root = Node(value)
-        else:
-            self._insert_recursive(self.root, value)
+class BST:
+    """A Binary Search Tree implementation."""
 
-    def _insert_recursive(self, node, value):
-        if value < node.value:
-            if node.left is None:
-                node.left = Node(value)
-            else:
-                self._insert_recursive(node.left, value)
-        else:
-            if node.right is None:
-                node.right = Node(value)
-            else:
-                self._insert_recursive(node.right, value)
+    def __init__(self, root_value=None):
+        """Initialize the BST with an optional root value."""
+        self.root = Node(root_value) if root_value is not None else None
 
-    def search(self, value):
-        return self._search_recursive(self.root, value)
-
-    def _search_recursive(self, node, value):
+    def insert(self, data, node=None):
+        """Insert a value into the BST."""
         if node is None:
-            return False
-        if node.value == value:
-            return True
-        elif value < node.value:
-            return self._search_recursive(node.left, value)
-        else:
-            return self._search_recursive(node.right, value)
-
-    def delete(self, value):
-        self.root = self._delete_recursive(self.root, value)
-
-    def _delete_recursive(self, node, value):
+            node = self.root
         if node is None:
+            self.root = Node(data)
+            return self.root
+
+        if data < node.value:
+            node.left = self.insert(data, node.left)
+        else:
+            node.right = self.insert(data, node.right)
+        return node
+
+    def search(self, target, node=None):
+        """Search for a target value in the BST."""
+        if node is None:
+            node = self.root
+        if node is None:
+            return None
+
+        if node.value == target:
             return node
-        if value < node.value:
-            node.left = self._delete_recursive(node.left, value)
-        elif value > node.value:
-            node.right = self._delete_recursive(node.right, value)
+
+        elif node.value < target:
+            return self.search(target, node.right)
         else:
-            # Node with only one child or no child
+            return self.search(target, node.left)
+
+    def get_min(self, node=None):
+        """Get the minimum value in the BST or subtree."""
+        if node is None:
+            node = self.root
+        if node is None:
+            return None
+
+        current = node
+        while current.left:
+            current = current.left
+        return current.value
+
+    def get_max(self, node=None):
+        """Get the maximum value in the BST or subtree."""
+        if node is None:
+            node = self.root
+        if node is None:
+            return None
+
+        current = node
+        while current.right:
+            current = current.right
+        return current.value
+
+    def find_height(self, node=None):
+        """Find the height of the BST or subtree."""
+        if node is None:
+            node = self.root
+        if node is None:
+            return -1
+
+        left_height = self.find_height(node.left)
+        right_height = self.find_height(node.right)
+
+        return 1 + max(left_height, right_height)
+
+    def get_min_node(self, node=None):
+        """Get the node with the minimum value in the BST or subtree."""
+        if node is None:
+            node = self.root
+        if node is None:
+            return None
+
+        current = node
+        while current.left:
+            current = current.left
+        return current
+
+    def delete(self, value, node=None):
+        """Delete a value from the BST using inorder successor."""
+        if node is None:
+            node = self.root
+        if node is None:
+            return None
+
+        if value < node.value:
+            node.left = self.delete(value, node.left)
+        elif value > node.value:
+            node.right = self.delete(value, node.right)
+        else:
+            # Node found
             if node.left is None:
                 return node.right
             elif node.right is None:
                 return node.left
-            # Node with two children: Get the inorder successor
-            temp = self._min_value_node(node.right)
-            node.value = temp.value
-            node.right = self._delete_recursive(node.right, temp.value)
+            else:
+                # Find inorder successor
+                succ = self.get_min_node(node.right)
+                node.value = succ.value
+                node.right = self.delete(succ.value, node.right)
         return node
 
-    def _min_value_node(self, node):
-        current = node
-        while current.left is not None:
-            current = current.left
-        return current
-
-    def preorder_traversal(self, node=None, traversal=""):
-        if node is None:
-            node = self.root
-        if node:
-            traversal += str(node.value) + " "
-            traversal = self.preorder_traversal(node.left, traversal)
-            traversal = self.preorder_traversal(node.right, traversal)
+    def preorder_traversal(self, start, traversal=""):
+        """Perform preorder traversal and return as string."""
+        if start:
+            traversal += str(start.value) + " "
+            traversal = self.preorder_traversal(start.left, traversal)
+            traversal = self.preorder_traversal(start.right, traversal)
         return traversal
 
-    def inorder_traversal(self, node=None, traversal=""):
-        if node is None:
-            node = self.root
-        if node:
-            traversal = self.inorder_traversal(node.left, traversal)
-            traversal += str(node.value) + " "
-            traversal = self.inorder_traversal(node.right, traversal)
+    def inorder_traversal(self, start, traversal=""):
+        """Perform inorder traversal and return as string."""
+        if start:
+            traversal = self.inorder_traversal(start.left, traversal)
+            traversal += str(start.value) + " "
+            traversal = self.inorder_traversal(start.right, traversal)
         return traversal
 
-    def postorder_traversal(self, node=None, traversal=""):
-        if node is None:
-            node = self.root
-        if node:
-            traversal = self.postorder_traversal(node.left, traversal)
-            traversal = self.postorder_traversal(node.right, traversal)
-            traversal += str(node.value) + " "
+    def postorder_traversal(self, start, traversal=""):
+        """Perform postorder traversal and return as string."""
+        if start:
+            traversal = self.postorder_traversal(start.left, traversal)
+            traversal = self.postorder_traversal(start.right, traversal)
+            traversal += str(start.value) + " "
         return traversal
